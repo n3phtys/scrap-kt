e10aa53b0293586ae784b925f28a445de79c8da9
nephtys <christopher.kaag@gmail.com>
2017-09-25 21:25:37 +0200

    added http server

--- src/main/kotlin/scrapperkt/Main.kt
+++ src/main/kotlin/scrapperkt/Main.kt
@@ -1,11 +1,33 @@
 package scrapperkt
 
+import org.jetbrains.ktor.routing.*
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.features.DefaultHeaders
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.features.*
+import org.jetbrains.ktor.host.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.netty.*
+import org.jetbrains.ktor.response.*
+import org.jetbrains.ktor.routing.*
 
 
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
     println("Hello, world!")
 
     val (request, response, result) = scrapManual()
     println("Call ended, result:")
     println(result.get())
+
+
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+
+
+    Thread.sleep(100000)
 }
